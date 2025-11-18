34af652aaeea2cd450556785fc40957cc3873453
Younghyo Park <younghyo@mit.edu>
2025-11-18 17:10:28 -0500

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,17 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+# Mock imports for packages that aren't available during doc build
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mujoco', 'mujoco.viewer', 'pylibfranka', 'ruckig']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
